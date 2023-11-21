037c08c66ebbc94a2a241fb1de44d62432094051
Gathorn-sys <maks.krachylo@gmail.com>
2023-11-21 17:45:18 +0200

    second commit

---
+++ ex1/ex1.cpp
@@ -0,0 +1,10 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+int main()
+{
+	char c = 'a',
+		s[10] = "123456789"; 
+	const char * t = "abcdefghi";
+	cout << c << '*' << endl << s << endl << t << endl << ">" << t[10] << "<";
+}
